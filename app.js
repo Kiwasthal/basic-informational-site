732b237d54b7fdffae7743ceaffdf57fc2f6f408
Steerpike <imthekoul@gmail.com>
2022-07-14 20:42:29 +0300

    Express navigation

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+app.listen(8080);
